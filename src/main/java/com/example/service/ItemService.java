8b5e647eb68647f23bab84b51e570e519cc7e9d9
Griotold <gotjd9773@naver.com>
2023-04-03 12:46:00 +0900

    6.1 상품 등록

---
+++ src/main/java/com/example/service/ItemService.java
@@ -0,0 +1,43 @@
+package com.example.service;
+
+import com.example.dto.ItemFormDto;
+import com.example.entity.Item;
+import com.example.entity.ItemImg;
+import com.example.repository.ItemImgRepository;
+import com.example.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+    private final ItemImgRepository itemImgRepository;
+    private final ItemImgService itemImgService;
+
+    public Long saveItem(ItemFormDto itemFormDto,
+                         List<MultipartFile> itemImgFileList) throws Exception {
+        // 상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        // 이미지 등록
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+            if (i == 0) {
+                itemImg.setRepImgYn("Y");
+            } else {
+                itemImg.setRepImgYn("N");
+            }
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+        return item.getId();
+    }
+}
