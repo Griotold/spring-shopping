ca2979f35e953a111fb632f18fb5d6015a974754
Griotold <gotjd9773@naver.com>
2023-04-03 18:21:16 +0900

    6.3 상품 관리(querydsl)

--- src/main/java/com/example/repository/ItemImgRepository.java
+++ src/main/java/com/example/repository/ItemImgRepository.java
@@ -1,11 +1,13 @@
 package com.example.repository;
 
+import com.example.entity.Item;
 import com.example.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long>{
 
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 }
